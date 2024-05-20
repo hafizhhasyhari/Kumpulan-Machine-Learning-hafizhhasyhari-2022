27bb04307df41e27a3404be3162415bbb4e93b11
Hafizh H Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2024-05-20 08:41:35 +0700

    Update finance_regression.py

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -35,3 +35,39 @@ feature_train, feature_test, target_train, target_test = train_test_split(featur
 train_color = "b"
 test_color = "r"
 
+### Regresi Anda mengarah ke sini!
+### Harap beri nama reg, sehingga kode plot di bawah mengambilnya dan
+### memplotnya dengan benar. Jangan lupa ubah test_color di atas dari "b" menjadi
+### "r" untuk membedakan titik latihan dengan titik tes.
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+print("Slope: ", reg.coef_)
+print("Interception : ", reg.intercept_)
+print("Score on Training set: ", reg.score(feature_train, target_train))
+print("Score on Test set: ", reg.score(feature_test, target_test))
+
+### menggambar diagram sebar, dengan titik pelatihan dan 
+import matplotlib.pyplot as plt
+for feature, target in zip(feature_test, target_test):
+    plt.scatter( feature, target, color=test_color ) 
+for feature, target in zip(feature_train, target_train):
+    plt.scatter( feature, target, color=train_color ) 
+
+
+### label untuk legenda
+plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
+plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
+
+### gambar garis regresi, setelah dikodekan mencoba:
+    plt.plot( feature_test, reg.predict(feature_test) )
+except NameError:
+    pass
+
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+print("Slope Outlier: ", reg.coef_)
+
+plt.xlabel(features_list[1])
+plt.ylabel(features_list[0])
+plt.legend()
+plt.show()
